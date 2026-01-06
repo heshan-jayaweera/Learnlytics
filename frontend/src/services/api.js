5dfb516dabcb5047b5fc4f5dc0fd30355dcb8c5e
Heshan141200 <it23765856@my.sliit.lk>
2026-01-06 13:42:45 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
-const API_URL = process.env.REACT_APP_API_URL || 'https://learnlytics-be.vercel.app/api';
+//const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://learnlytics-be.vercel.app';
 
 // Create axios instance
 const api = axios.create({
